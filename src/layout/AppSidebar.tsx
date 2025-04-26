bb7ad7a8e0023336c187ce824270eeee3f159e37
shuvadeepmondal <121492365+shuvadeepmondal@users.noreply.github.com>
2025-04-26 23:02:17 +0530

    Changes

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -241,7 +241,7 @@ const AppSidebar: React.FC = () => {
         <Link to="/">
           {isExpanded || isHovered || isMobileOpen ? (
             <>
-              <h1 className="text-2xl dark:text-gray-200 font-bold">Project SAGE</h1>
+              <h1 className="text-2xl dark:text-gray-200 font-bold">SAGE</h1>
             </>
           ) : (
         <h2 className="text-2xl dark:text-gray-200 font-bold">SAGE</h2>
